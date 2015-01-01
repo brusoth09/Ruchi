9a472bd50b9818bd0051293a9de3ebc9c5e7363a
Burusothman <brusoth09@gmail.com>
2015-01-01 22:06:13 +0530

    Restaurant Model modified

--- src/com/ruchi/engine/models/Restaurant.java
+++ src/com/ruchi/engine/models/Restaurant.java
@@ -1,6 +1,6 @@
 package com.ruchi.engine.models;
 
-import com.ruchi.engine.preprocessing.Stemming;
+import com.ruchi.engine.preprocessing.Stemmer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,18 +16,31 @@ public class Restaurant {
     private HashMap<String,Integer> food_map=new HashMap<String,Integer>();
 
     public Restaurant(String name){
-    	this.name=name;
+    	this.setName(name);
     }
+    
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
     public void addReview(Review object){
         review_list.add(object);
     }
-    
+
     public ArrayList<Review> getReview(){
     	return review_list;
     }
 
     public void addFood(String food){
-        String stemmed_food= Stemming.pluralToSingular(food).toLowerCase();
+        String stemmed_food= Stemmer.pluralToSingular(food).toLowerCase();
 
         if(food_map.containsKey(stemmed_food)){
             food_map.put(stemmed_food,food_map.get(stemmed_food)+1);
@@ -36,4 +49,5 @@ public class Restaurant {
             food_map.put(stemmed_food,1);
         }
     }
+	
 }
