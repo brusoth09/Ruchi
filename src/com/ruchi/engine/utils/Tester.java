9ddbd126f218c1b10706a45f379d073804a53530
Burusothman <brusoth09@gmail.com>
2015-01-02 13:38:24 +0530

    int array added

--- src/com/ruchi/engine/utils/Tester.java
+++ src/com/ruchi/engine/utils/Tester.java
@@ -108,7 +108,7 @@ public class Tester {
             {
                 if(fea.contains(iter.next())){
                     output=output.concat(fea).concat(",");
-                    line.addFood(fea,0);
+                    line.addFood(fea,new Integer[]{0,0});
                     wc.addFood(fea.toLowerCase());
                     iter.remove();
                     break;
@@ -122,7 +122,7 @@ public class Tester {
         {
             String next=iter.next();
             output=output.concat(next).concat(",");
-            line.addFood(next,0);
+            line.addFood(next,new Integer[]{0,0});
             wc.addFood(next);
         }
         list.add(line);
