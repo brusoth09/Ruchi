dca08105a95bfde712beefc2a9bad7620866c66b
ThamayanthyS <thamaya16@gmail.com>
2015-01-06 14:03:41 +0530

    DataStore Completed.

--- src/com/ruchi/engine/database/DataStore.java
+++ src/com/ruchi/engine/database/DataStore.java
@@ -1,6 +1,9 @@
 package com.ruchi.engine.database;
 
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -8,6 +11,7 @@ import org.apache.lucene.queries.function.valuesource.ReciprocalFloatFunction;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.ruchi.hibernate.model.DAO.CityDao;
 import com.ruchi.hibernate.model.DAO.FoodDao;
@@ -17,12 +21,18 @@ import com.ruchi.hibernate.model.DAO.ReviewDao;
 import com.ruchi.hibernate.model.DAO.ReviewFoodDao;
 import com.ruchi.hibernate.util.HibernateUtil;
 
+import edu.stanford.nlp.time.SUTime.Time;
+
 public class DataStore {
 	public static void main(String[] args) {
 		DataStore dataStore = new DataStore();
 		// dataStore.insertReviewRating("1408182490042", 4f);
 		// dataStore.insertRestRating("111", 2f);
-		dataStore.insertFood("sample food");
+
+//		System.out.println(dataStore.insertFood("sample food6"));
+//		System.out.println(dataStore.insertReviewFood("1408182490041", "2015-01-06 10:16:28", 4f));
+		System.out.println(dataStore.insertRestFood("1408181771009", "2015-01-06 10:16:28", 1f));
+		System.out.println("done");
 	}
 
 	public boolean insertReviewRating(String review_id, float rating) {
@@ -68,28 +78,20 @@ public class DataStore {
 	}
 
 	public Date insertFood(String food_name) {
-		Session session = null;
-		try {
-			session = HibernateUtil.getSessionFactory().getCurrentSession();
-			session.beginTransaction();
-			FoodDao foodDao;// = (FoodDao) session.get(FoodDao.class,
-							// food_name);
-			session.getTransaction().commit();
 
+		try {
 			Timestamp food_id = getFood_id(food_name);
+
+			// Transacstion transaction;
 			if (!(food_id == null)) {
 				return food_id;
-			} else if (food_id == null) {
-				foodDao = new FoodDao();
-				foodDao.setFood_name(food_name);
-				session.save(foodDao);
-				session.getTransaction().commit();
-				Timestamp food_id_new = getFood_id(food_name);
-				return food_id_new;
+			} else {
+				 insertNewFood(food_name);
+				 return getFood_id(food_name);
 			}
 
 		} catch (HibernateException e) {
-			session.close();
+			// session.close();
 			e.printStackTrace();
 		}
 		return null;
@@ -99,19 +101,24 @@ public class DataStore {
 			float rating) {
 
 		Session session = null;
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		final Transaction transaction = session.beginTransaction();
 		try {
-			session = HibernateUtil.getSessionFactory().getCurrentSession();
-			session.beginTransaction();
+
 			ReviewFoodDao reviewFoodDao = new ReviewFoodDao();
 			reviewFoodDao.setReview_id(review_id);
-			reviewFoodDao.setFood_id(food_id);
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		    Date parsedDate = dateFormat.parse(food_id);
+		    Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+			reviewFoodDao.setFood_id(timestamp);
 			reviewFoodDao.setRating(rating);
 			session.save(reviewFoodDao);
 			session.getTransaction().commit();
 			return true;
 
-		} catch (HibernateException e) {
-			session.close();
+		} catch (HibernateException | ParseException e) {
+			transaction.rollback();
+			
 			e.printStackTrace();
 		}
 		return false;
@@ -124,16 +131,19 @@ public class DataStore {
 			session.beginTransaction();
 			RestaurantFoodDao restaurantFoodDao = new RestaurantFoodDao();
 			restaurantFoodDao.setRest_id(rest_id);
-			restaurantFoodDao.setFood_id(food_id);
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		    Date parsedDate = dateFormat.parse(food_id);
+		    Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+			restaurantFoodDao.setFood_id(timestamp);
 			restaurantFoodDao.setRating(rating);
 			session.save(restaurantFoodDao);
 			session.getTransaction().commit();
 			return true;
 
-		} catch (HibernateException e) {
-			session.close();
+		} catch (HibernateException | ParseException e) {
+//			session.close();
 			e.printStackTrace();
-		}
+		} 
 		return false;
 	}
 
@@ -143,17 +153,49 @@ public class DataStore {
 			session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
 			FoodDao foodDao;
-			String hql = "FROM FoodDao F WHERE F.food_name = '" + food_name+"'";
+			String hql = "FROM FoodDao F WHERE F.food_name = '" + food_name
+					+ "'";
 			Query query = session.createQuery(hql);
+			@SuppressWarnings("unchecked")
 			List<FoodDao> results = query.list();
 			if (!results.isEmpty()) {
+				session.close();
+//				System.out.println("inserted"+results.get(0).getFood_id());
 				return results.get(0).getFood_id();
-			}
+			}	
 			//
 		} catch (HibernateException e) {
-			session.close();
+//			session.close();
 			e.printStackTrace();
 		}
+		session.close();
+		return null;
+	}
+
+	private Timestamp insertNewFood(String food_name) {
+		Session session = null;
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+
+			session.beginTransaction();
+			FoodDao foodDao;// = (FoodDao) session.get(FoodDao.class,
+							// food_name);
+			foodDao = new FoodDao();
+			foodDao.setFood_name(food_name);
+			Timestamp timestamp=new Timestamp(Calendar.getInstance()
+					.getTimeInMillis());
+			foodDao.setFood_id(timestamp);
+			session.save(foodDao);
+			session.getTransaction().commit();
+//			Timestamp food_id_new = getFood_id(food_name);
+//			System.out.println("inserted"+food_id_new);
+//			session.close();
+			return timestamp;
+		} catch (HibernateException exception) {
+			
+			exception.printStackTrace();
+		}
+		session.close();
 		return null;
 	}
 }
--- src/com/ruchi/hibernate/model/DAO/ReviewFoodDao.java
+++ src/com/ruchi/hibernate/model/DAO/ReviewFoodDao.java
@@ -1,5 +1,7 @@
 package com.ruchi.hibernate.model.DAO;
 
+import java.sql.Timestamp;
+
 import javax.persistence.*;
 
 /**
@@ -18,7 +20,7 @@ public class ReviewFoodDao {
     private String review_id;
 
     @Column(name = "food_id", nullable = false, unique = false, length = 50)
-    private String food_id;
+    private Timestamp food_id;
 
     @Column(name = "rating", nullable = false, unique = false, length = 50)
     private float rating;
@@ -30,14 +32,14 @@ public class ReviewFoodDao {
     public void setReview_id(String review_id) {
         this.review_id = review_id;
     }
+   
+	public Timestamp getFood_id() {
+		return food_id;
+	}
 
-    public String getFood_id() {
-        return food_id;
-    }
-
-    public void setFood_id(String food_id) {
-        this.food_id = food_id;
-    }
+	public void setFood_id(Timestamp food_id) {
+		this.food_id = food_id;
+	}
 
 	public float getRating() {
 		return rating;
