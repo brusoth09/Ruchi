260f163e0b14b6ff170bdd091a35b1bf54bf90ed
Burusothman <brusoth09@gmail.com>
2015-02-03 23:02:45 +0530

    changed as static

--- src/com/ruchi/engine/mapper/Mapper.java
+++ src/com/ruchi/engine/mapper/Mapper.java
@@ -10,13 +10,13 @@ import com.ruchi.hibernate.model.DAO.RestaurantDao;
 import com.ruchi.hibernate.model.DAO.ReviewDao;
 
 public class Mapper {
-	DataStore dataStore;
+	static DataStore dataStore;
 
-	private Mapper() {
+	static{
 		dataStore = new DataStore();
 	}
 
-	public ArrayList<String> getRestaurantReviews(String res_name) {
+	public static ArrayList<String> getRestaurantReviews(String res_name) {
 		ArrayList<String> rest_reviews = new ArrayList<String>();
 		List<ReviewDao> daos = dataStore.getReviewsByRestName(res_name);
 		for (ReviewDao r : daos) {
@@ -26,7 +26,7 @@ public class Mapper {
 		return rest_reviews;
 	}
 	
-	public ArrayList<String> getRestaurantNames(){
+	public static ArrayList<String> getRestaurantNames(){
 		ArrayList<String> rest_names = new ArrayList<String>();
 		List<RestaurantDao> daos = dataStore.getRestaurantNames();
 		for(RestaurantDao r:daos){
@@ -35,7 +35,7 @@ public class Mapper {
 		return rest_names;
 	}
 	
-	public ArrayList<String> getRestaurantIDs () {
+	public static ArrayList<String> getRestaurantIDs () {
 		ArrayList<String> rest_ids = new ArrayList<String>();
 		List<RestaurantDao> daos = dataStore.getRestaurantIds();
 		for(RestaurantDao r:daos){
@@ -44,7 +44,7 @@ public class Mapper {
 		return rest_ids;		
 	}
 	
-	public ArrayList<String> getRestaurantReview(String rest_id) {
+	public static ArrayList<String> getRestaurantReview(String rest_id) {
 		ArrayList<String> rest_reviews = new ArrayList<String>();
 		List<ReviewDao> daos = dataStore.getReviewsByRestId(rest_id);
 		for(ReviewDao r:daos){
@@ -53,30 +53,30 @@ public class Mapper {
 		return rest_reviews;		
 	}
 	
-	public void removeFood(String food_name) {
+	public static void removeFood(String food_name) {
 		dataStore.removeFoodName(food_name);
 	}
 	
-	public void getFoodNames(ArrayList<String> dictionary){
+	public static void getFoodNames(ArrayList<String> dictionary){
 		List<FoodDao> daos = dataStore.getFoodNames();
 		for(FoodDao r:daos){
 			dictionary.add(r.getFood_name());
 		}
 	}
 	
-	public void insertFood(String food_name){
+	public static void insertFood(String food_name){
 		dataStore.insertFood(food_name);
 	}
 		
-	public void insertFoodInit(String food_name){
+	public static void insertFoodInit(String food_name){
 		dataStore.insertFoodInit(food_name);
 	}
 	
-	public void removeFoodInit(String food_name) {
+	public static void removeFoodInit(String food_name) {
 		dataStore.removeFoodInitName(food_name);
 	}
 	
-	public void getFoodInitNames(ArrayList<String> dictionary){
+	public static void getFoodInitNames(ArrayList<String> dictionary){
 		List<FoodInitDao> daos = dataStore.getFoodInitNames();
 		for(FoodInitDao r:daos){
 			dictionary.add(r.getFood_name());
