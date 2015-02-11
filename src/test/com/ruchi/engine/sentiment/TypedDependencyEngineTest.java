eb6a7a4d39167f9e125d2cc844fe14c68b720b07
sanjithuom <sanjith4@gmail.com>
2015-02-11 22:52:06 +0530

    Test and Comment added to sentiment system

--- src/test/com/ruchi/engine/sentiment/TypedDependencyEngineTest.java
+++ src/test/com/ruchi/engine/sentiment/TypedDependencyEngineTest.java
@@ -1,25 +1,24 @@
 package test.com.ruchi.engine.sentiment;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.ruchi.engine.foodextractionEvaluvation.ActualIdentifier;
 import com.ruchi.engine.models.Sentence;
 import com.ruchi.engine.sentiment.TypedDependencyEngine;
 
 public class TypedDependencyEngineTest {
+	TypedDependencyEngine typedDependencyEngine;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		new TypedDependencyEngine();
 	}
 
 	@Before
@@ -32,50 +31,46 @@ public class TypedDependencyEngineTest {
 
 	@Test
 	public void testTypedDependencyEngine() {
-		
+
 	}
 
 	@Test
 	public void testMain() {
-	
+
 	}
 
 	@Test
 	public void testFoodSentiment() {
-		TypedDependencyEngine typedDependencyEngine	=new TypedDependencyEngine();
-		Sentence sentence=mock(Sentence.class);
-		when(sentence.getSentence()).thenReturn("test");
-		HashMap<String, Integer[]> foodMap = mock(HashMap.class);
-		when(sentence.getFoodMap()).thenReturn(foodMap);		;
-		Set<String> keySet = mock(Set.class);
-		when(foodMap.keySet()).thenReturn(keySet);
-		when(sentence.getSentence()).thenReturn("test");
-		typedDependencyEngine.foodSentiment(sentence);
+		String inText = "pizza was awesome and chinese pasta was awful";
+		Sentence sentence = new Sentence(inText);
+		Integer[] location = new Integer[2];
+		location[0] = 0;
+		location[1] = 1;
+		Integer[] location2 = new Integer[2];
+		location2[0] = 4;
+		location2[1] = 2;
+		sentence.addFood("pizza", location);
+		sentence.addFood("pasta", location2);
+		HashMap<String, Double> foodSentiment = TypedDependencyEngine.foodSentiment(sentence);
+		Set<String> keySet = foodSentiment.keySet();
+		for (String key : keySet) {
+			if(key.equals("pizza")){
+				Assert.assertEquals(5.0, (double) foodSentiment.get(key));
+			}
+			else{
+				Assert.assertEquals(1.0, (double) foodSentiment.get(key));
+			}
+		}
 	}
 
 	@Test
 	public void testSentimentAnalysis() {
-	
-	}
-
-	@Test
-	public void testSentiTyped() {
-		
-	}
-
-	@Test
-	public void testGenerateTypedDependency() {
-		
-	}
-
-	@Test
-	public void testSegmentText() {
-		
-	}
-
-	@Test
-	public void testGenerateSentence() {
-	
+		String inText = "pizza was awesome";
+		String inText2 = "chinese pasta was awful";
+		ArrayList<String> sent = TypedDependencyEngine.sentimentAnalysis(inText);
+		Assert.assertEquals("positive", sent.get(0));
+		sent = TypedDependencyEngine.sentimentAnalysis(inText2);
+		Assert.assertEquals("negative", sent.get(0));
 	}
 
 }
