4b11c64df182acfc3a2421797386e65d9124b515
ThamayanthyS <thamaya16@gmail.com>
2015-02-12 12:56:58 +0530

    test cases added for datastore and mapper

---
+++ src/test/com/ruchi/engine/mapper/MapperTest.java
@@ -0,0 +1,119 @@
+package test.com.ruchi.engine.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapperTest {
+
+	@Test
+	public void testGetRestaurantReviewsByRestName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsAndIdsByRestName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsByRestNameTrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsAndIdsByRestNameTrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantIDs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsByRestId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsAndIdsByRestId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsByRestIdTrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestaurantReviewsAndIdsByRestIdTrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFoodNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertFood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertFoodInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFoodInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFoodInitNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertReviewFood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertRestFood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertReviewRating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertRestRating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRestName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllRestaurantIdsAndNames() {
+		fail("Not yet implemented");
+	}
+
+}
